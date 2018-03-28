f4abf2721bb570d897fafa7a77f23e151821b2de
LeCray <mistercryptogrid@gmail.com>
2018-03-28 10:51:43 +0200

    Can log invoices from server

--- android/components/Actions/Invoices.js
+++ android/components/Actions/Invoices.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Platform, StyleSheet, Dimensions, Text, View} from 'react-native';
+import {Platform, StyleSheet, Dimensions, Text, View, Modal} from 'react-native';
 import Pdf from 'react-native-pdf';
 
 
@@ -11,10 +11,19 @@ export class Invoices extends Component {
 		title: 'Invoices'
 	}
 
-/*
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: "", 
+			invoices: [],
+			modalVisible: false
+		};
+	}
+
+
 	componentWillMount() {
 
-		fetch("http://10.199.253.13:3000/api/v1/invoices", {
+		fetch("http://192.168.43.42:3000/api/v1/get_invoices", {
 			method: "POST", 
 			headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
 			body: JSON.stringify({
@@ -22,32 +31,50 @@ export class Invoices extends Component {
 			}), 
         })
         .then((responseData) => {
-			console.log('Booking Cancelled')
-			ToastAndroid.show('Booking Request Cancelled', ToastAndroid.LONG);
+			console.log('Below is respose from mobile')
+			this.setState({invoices: responseData._bodyText})
+			console.log(this.state.invoices)
         })
         .catch((error) => {
           console.error(error);
         })
         .done();
 	}
-*/
+
+	_showModal = () => this.setState({ modalVisible: true })
+	_hideModal = () => this.setState({ modalVisible: false })
+
 	render() {
 		const source = {uri:'https://www.tke.org/files/file/The_48_Laws_of_Power.pdf'};
 		return(  
 			<View style={styles.container}>
 				
-				<Pdf
-                    source={source}
-                    onLoadComplete={(numberOfPages,filePath)=>{
-                        console.log('number of pages: ', numberOfPages);
-                    }}
-                    onPageChanged={(page,numberOfPages)=>{
-                        console.log('current page: ', page);
-                    }}
-                    onError={(error)=>{
-                        console.log(error);
-                    }}
-                    style={styles.pdf}/>
+				<Text onPress={this._showModal}> The_48_Laws_of_Power.pdf </Text>
+
+                <Modal
+				animationType="slide"
+				transparent={true}
+				visible = {this.state.modalVisible}
+				onRequestClose={this._hideModal}>
+					
+					<View style={{paddingTop: 50, backgroundColor: '#00000080', flex: 1}}>
+						<View  style={{backgroundColor: '#fff', flex: 1}}>
+							<Pdf
+			                    source={source}
+			                    onLoadComplete={(numberOfPages,filePath)=>{
+			                        console.log('number of pages: ', numberOfPages);
+			                    }}
+			                    onPageChanged={(page,numberOfPages)=>{
+			                        console.log('current page: ', page);
+			                    }}
+			                    onError={(error)=>{
+			                        console.log(error);
+			                    }}
+			                    style={styles.pdf}/>
+						</View>
+					</View>
+
+		        </Modal>
 			</View>
 		)
 	}
